71c038bae386e99db3326855acc7011802c13524
ahmetveburak <ahmetbozyurtt@gmail.com>
2020-07-22 23:27:56 +0300

    SSH db connection added

--- prodil/__main__.py
+++ prodil/__main__.py
@@ -1,20 +1,17 @@
 import mongoengine
+from sshtunnel import SSHTunnelForwarder
 
 # from mongoengine import document
 from prodil.models.model import Documents, Links, Books, Questions
 from prodil.BotConfig import ProDil
+import configparser
 import logging
 from decouple import config
 
-# logging.basicConfig(
-#     format="%(levelname)s - %(name)s - %(message)s",
-#     level=logging.getLevelName(config("LOG_LEVEL", default="INFO")),
-# )
+logging.basicConfig(
+    format="%(levelname)s - %(name)s - %(message)s",
+    level=logging.getLevelName(config("LOG_LEVEL", default="INFO")),
+)
 
-
-mongoengine.register_connection(alias="core", name="prodil_test")
-
-
-# path = os.getcwd()
 if __name__ == "__main__":
     ProDil().run()
