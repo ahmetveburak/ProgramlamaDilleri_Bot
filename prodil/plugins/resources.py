a9d45dbf74170762fdb4241e8745d9ef0c503cff
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-04-30 16:32:33 +0300

    feat resource uploading
    add resource from admin site and update it from private telegram chat. license appended

--- prodil/plugins/resources.py
+++ prodil/plugins/resources.py
@@ -12,13 +12,14 @@ atropos = filters.create(lambda _, __, msg: msg.from_user.id in [635568322])
 @ProDil.on_message(atropos & filters.media & filters.private)
 async def book_update(client: Client, message: Message):
     # TODO
-    # Add new ebooks
+    print(message)
     book = session.query(Document).filter(Document.path == message.document.file_name).first()
 
     if book:
         book.file_id = message.document.file_id
-        print("GUNCELLENDI:    ", message.document.file_name)
+        book.enabled = True
+        await message.reply_text(text="Kaynak güncellendir ve aktifleştirildi.")
         session.commit()
 
     else:
-        print("GUNCELLENEMEDI: ", message.document.file_name)
+        await message.reply_text(text="Kaynak güncellenemedi. Veritabanında böyle bir dosya bulunamadı.")
