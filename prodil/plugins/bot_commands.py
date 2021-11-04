6bf23b4ae64db16c3e7d9200387148a442c5ad8b
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-11-05 01:20:04 +0300

    add feat update uploaded files

--- prodil/plugins/bot_commands.py
+++ prodil/plugins/bot_commands.py
@@ -1,12 +1,15 @@
+import asyncio
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from prodil.BotConfig import ProDil
 from prodil.utils.helpers import command
+from prodil_client.client import api
 
 
 @ProDil.on_message(command("hakkinda") & filters.private)
-async def test(client: Client, message: Message):
+async def bot_about(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text=(
@@ -31,3 +34,20 @@ async def test(client: Client, message: Message):
             ]
         ),
     )
+
+
+@ProDil.on_message(filters.me & filters.media & filters.private)
+async def bot_file_update(_: Client, message: Message):
+    update = api.update_resource(
+        file_name=message.document.file_name,
+        file_size=message.document.file_size,
+        file_id=message.document.file_id,
+    )
+
+    text = "Dosya guncellenemedi."
+    if update.status_code == 200:
+        text = "Dosya basariyla guncellendi"
+
+    reply_message = await message.reply_text(text=f"`{text}`")
+    await asyncio.sleep(3)
+    await reply_message.delete()
