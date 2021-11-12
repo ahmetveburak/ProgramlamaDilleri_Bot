b319e7acae7718a3f22a1a9d14b32586a524acb0
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-11-12 22:38:49 +0300

    minor refactor

--- prodil/BotConfig.py
+++ prodil/BotConfig.py
@@ -1,34 +1,25 @@
 from pyrogram import Client
 from pyrogram.types import Message
 
-# TODO package and install from github
-from prodil_client.prodil_client.client import ProdilAPI
-
-api = ProdilAPI(
-        api_url="http://127.0.0.1:8000/api",
-        email="admin@admin.com",
-        password="1",
-    )
+from client.api import ProdilAPI
 
 
 class ProDil(Client, Message):
-    OWNER_ID: int = 635568322
-    CHATS: list = []
-
     def __init__(self):
         name = self.__class__.__name__.lower()
         super().__init__(
             session_name=name,
-            config_file=f"{name}/{name}.ini",
+            config_file=f"{name}.ini",
             workers=8,
             plugins=dict(root=f"{name}/plugins"),
             workdir=f"{name}",
         )
-
-        self.admins = {chat: {ProDil.OWNER_ID} for chat in ProDil.CHATS}
 
     async def start(self):
         await super().start()
 
     async def stop(self, *args):
         await super().stop()
+
+
+api = ProdilAPI(config_file="prodil.ini")
