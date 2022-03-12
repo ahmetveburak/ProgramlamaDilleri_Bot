e557b5888d77a8ac2256808d5cfced61f47771aa
ahmetveburak <ahmetbozyurtt@gmail.com>
2022-03-12 23:45:30 +0300

    add logger generator

--- prodil/__main__.py
+++ prodil/__main__.py
@@ -1,21 +1,4 @@
-import logging
-from os import makedirs
-from os.path import join
-
 from prodil.BotConfig import ProDil
-
-log_dir = "logs"
-try:
-    makedirs(log_dir)
-except FileExistsError:
-    pass
-
-logger = logging.getLogger()
-fh = logging.FileHandler(join(log_dir, "prodil.log"))
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
 
 if __name__ == "__main__":
     ProDil().run()
---
+++ prodil/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+from os import makedirs
+from os.path import exists, join
+
+
+def get_logger(name=None, file_name="prodil.log", level=logging.WARNING, log_dir="logs"):
+    if not exists(log_dir):
+        makedirs(log_dir)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(join(log_dir, file_name))
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    fh.setFormatter(formatter)
+    fh.setLevel(level)
+    logger.addHandler(fh)
+
+    return logger
